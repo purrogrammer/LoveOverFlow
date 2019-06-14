e9a753912eaa7e405602e9641ec80fd14fccc8db
Kennedy777 <reach.marguerite@gmail.com>
2019-06-14 16:41:14 -0700

    buildout of component files and code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewQuestionComponent } from './new-question/new-question.component';
+import { EditQuestionComponent } from './edit-question/edit-question.component';
+import { QuestionListComponent } from './question-list/question-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewQuestionComponent,
+    EditQuestionComponent,
+    QuestionListComponent
   ],
   imports: [
     BrowserModule
